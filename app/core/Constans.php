9ae6bfc9d803ec366207d480214599b43a512b2c
Jauhar N <jezishwa@gmail.com>
2021-06-17 11:51:46 +0700

    Menambahkan Template stisla

--- app/core/Constans.php
+++ app/core/Constans.php
@@ -1,8 +1,8 @@
 <?php
 
-define('BASEURL', 'https://localhost/ppmawar');
+define('BASEURL', 'https://ppdb.itmamawar.id');
 date_default_timezone_set('Asia/Jakarta');
 define('URL', 'localhost');
-define('USER_DB', 'root');
-define('PASS_DB', '');
-define('NAMA_DB', 'ppmawar');
+define('USER_DB', 'itmf4474_jauhar');
+define('PASS_DB', 'mawar100%');
+define('NAMA_DB', 'itmf4474_ppdb');
